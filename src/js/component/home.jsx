dbe70a3a6622f4b5340a58263a82f24f5e4f4c0b
MatiRosas31 <165863634+MatiRosas31@users.noreply.github.com>
2024-10-08 00:15:25 +0000

    [Projecto completado] -Se creo con css la parte de estilo/visual -En el archivo home se uso un hookstate que tiene una variable que se llama "selectedDiv" -Cuando se hace click "selectedDiv" pasa a ser un color. Uno de los colores -green, yellow o red- -Luego se evalua si selectedDiv es uno de los colores y de ser asi se agrega la clase selected como template literal

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,39 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [selectedDiv, setSelectedDiv] = useState(""); 
+    return (
+        <>
+            <div className="rectanguloSmall"></div>
+            <div className="rectanguloBig">
+                <div
+                    className={`circleGreen bg-success ${
+                        selectedDiv === "green" ? "selected" : ""
+                    }`}
+                    onClick={() => {
+                        setSelectedDiv(selectedDiv === "green" ? "" : "green");
+                    }}
+                ></div>
+                <div
+                    className={`circleYellow bg-warning ${
+                        selectedDiv === "yellow" ? "selected" : ""
+                    }`}
+                    onClick={() => {
+                        setSelectedDiv(selectedDiv === "yellow" ? "" : "yellow");
+                    }}
+                ></div>
+                <div
+                    className={`circleRed bg-danger ${
+                        selectedDiv === "red" ? "selected" : ""
+                    }`}
+                    onClick={() => {
+                        setSelectedDiv(selectedDiv === "red" ? "" : "red");
+                    }}
+                ></div>
+            </div>
+        </>
+    );
 };
 
 export default Home;
